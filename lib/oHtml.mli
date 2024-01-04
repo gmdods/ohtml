01a100e860f4f8b668b95f79b44c7eca62c52095
gmd01bit <gmd01bit@gmail.com>
2024-01-04 18:54:40 +0200

    :memo: Adding mli and better interface

---
+++ lib/oHtml.mli
@@ -0,0 +1,14 @@
+type t
+
+(* Lift string into managed HTML *)
+val html : string -> t
+
+(* Lower HTML into string *)
+val print : t -> string
+
+(* Create an enclosed HTML tag *)
+val tag : string -> ?attr:string -> t list -> t
+
+(* Create div tag *)
+val div : ?classes: string list -> t list -> t
+
